c36d5561f3586d60518163fe72fb70958782b924
Jessica Wolff <jleahwolff@gmail.com>
2019-12-12 23:57:02 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import "./App.css";
 import React, {useState, useEffect} from "react";
 import NasaCard from './Components/aCard';
 import axios from 'axios';
+import styled from 'styled-components';
+import './index.css'
+
+const PageDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: #FFDEE9;
+background-image: linear-gradient(0deg, #FFDEE9 0%, #d5fffe 100%);
+color: black;
+
+h1{
+  font-family: 'Comfortaa', cursive;
+  margin: 2%;
+  padding: 1rem;
+  background: dodgerblue;
+  color: black;
+  border-radius: 100% 4%;
+}
+
+p{
+  background: white;
+  border-radius: 2%;
+  padding: 1rem;
+}
+`
 
 //function
 function App() {
@@ -16,7 +42,7 @@ function App() {
 
   //useEffect
   useEffect(() => {
-    axios
+  axios
       .get(
         `https://api.nasa.gov/planetary/apod?api_key=Mb4j5wTSo0YWH6vZnKzyHM0H1wSSDNq3e2Txhfz1`
         )
@@ -32,7 +58,7 @@ function App() {
   }, [dailyDate])
 
   return (
-      <div className="App">
+      <PageDiv className="App">
         <h1>NASA Photo of the Day</h1>
           <NasaCard 
           dailyPhoto = {dailyPhoto}
@@ -41,7 +67,7 @@ function App() {
           dailyDesc = {dailyDesc}
           changeDate={() => changeDate(document.getElementById('date').value)}
           />
-      </div>
+      </PageDiv>
   )
 }
 
